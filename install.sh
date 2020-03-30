314202660b376c11abc6f7750457b736d2bc0317
Gerald Manipon <pymonger@gmail.com>
2020-03-30 09:36:21 -0700

    build from develop-es1 branches

--- install.sh
+++ install.sh
@@ -275,7 +275,7 @@ cd $OPS
 # install dev environment
 if [[ "$DEV" == 1 ]]; then
   # clone prov_es package
-  install_dev_repo $OPS prov_es https://github.com/hysds/prov_es.git
+  install_dev_repo $OPS prov_es https://github.com/hysds/prov_es.git develop-es1
   
   
   # clone osaka package
@@ -286,13 +286,13 @@ if [[ "$DEV" == 1 ]]; then
   
   
   # clone hysds_commons package
-  install_dev_repo $OPS hysds_commons https://github.com/hysds/hysds_commons.git
+  install_dev_repo $OPS hysds_commons https://github.com/hysds/hysds_commons.git develop-es1
   
   
   # clone hysds package
   cd $OPS
   PACKAGE=hysds
-  clone_dev_repo $OPS $PACKAGE https://github.com/hysds/hysds.git
+  clone_dev_repo $OPS $PACKAGE https://github.com/hysds/hysds.git develop-es1
   cd $OPS/$PACKAGE
   pip install -e .
   if [ "$?" -ne 0 ]; then
@@ -306,31 +306,31 @@ if [[ "$DEV" == 1 ]]; then
   
   
   # clone chimera package
-  install_dev_repo $OPS chimera https://github.com/hysds/chimera.git
+  install_dev_repo $OPS chimera https://github.com/hysds/chimera.git develop-es1
   
   
   # clone mozart package
-  install_dev_repo $OPS mozart https://github.com/hysds/mozart.git
+  install_dev_repo $OPS mozart https://github.com/hysds/mozart.git develop-es1
   
   
   # clone figaro package
-  install_dev_repo $OPS figaro https://github.com/hysds/figaro.git
+  install_dev_repo $OPS figaro https://github.com/hysds/figaro.git develop-es1
   
   
   # clone sdscli package
-  install_dev_repo $OPS sdscli https://github.com/sdskit/sdscli.git
+  install_dev_repo $OPS sdscli https://github.com/sdskit/sdscli.git develop-es1
   
   
   # clone grq2 package
-  install_dev_repo $OPS grq2 https://github.com/hysds/grq2.git
+  install_dev_repo $OPS grq2 https://github.com/hysds/grq2.git develop-es1
   
   
   # clone tosca package
-  install_dev_repo $OPS tosca https://github.com/hysds/tosca.git
+  install_dev_repo $OPS tosca https://github.com/hysds/tosca.git develop-es1
   
   
   # clone pele package
-  install_dev_repo $OPS pele https://github.com/hysds/pele.git
+  install_dev_repo $OPS pele https://github.com/hysds/pele.git develop-es1
   
   
   # clone spyddder-man package
@@ -338,11 +338,11 @@ if [[ "$DEV" == 1 ]]; then
   
   
   # clone lightweight-jobs package
-  clone_dev_repo $OPS lightweight-jobs https://github.com/hysds/lightweight-jobs.git
+  clone_dev_repo $OPS lightweight-jobs https://github.com/hysds/lightweight-jobs.git develop-es1
   
   
   # clone container-builder package
-  clone_dev_repo $OPS container-builder https://github.com/hysds/container-builder.git
+  clone_dev_repo $OPS container-builder https://github.com/hysds/container-builder.git develop-es1
   
   
   # clone s3-bucket-listing package
@@ -350,7 +350,7 @@ if [[ "$DEV" == 1 ]]; then
   
   
   # clone hysds-dockerfiles package
-  clone_dev_repo $OPS hysds-dockerfiles https://github.com/hysds/hysds-dockerfiles.git
+  clone_dev_repo $OPS hysds-dockerfiles https://github.com/hysds/hysds-dockerfiles.git develop-es1
   
   
   # clone hysds-cloud-functions package
