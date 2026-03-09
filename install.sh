954dc6aa598bbb1e175e2deaaaafff0559f13a0d
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2026-03-09 09:41:49 -0700

    kludge

--- install.sh
+++ install.sh
@@ -370,7 +370,7 @@ if [[ "$DEV" == 1 ]]; then
 
   # download latest develop verdi image
   if [[ "$COMPONENT" == "mozart" ]]; then
-    ${BASE_PATH}/download_latest.py $API_URL hysds hysds-dockerfiles -o ${INSTALL_DIR}/pkgs -r "^hysds-verdi-develop"
+    ${BASE_PATH}/download_latest.py $API_URL hysds hysds-dockerfiles -o ${INSTALL_DIR}/pkgs -r "^hysds-verdi-HC-567"
   fi
 else
   # print release if not specified
