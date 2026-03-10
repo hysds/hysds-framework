5a76ba28f302f135a77c3d10fd363cbe3d070fc0
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2026-03-10 07:37:33 -0700

    check out sdscli HC-567

--- install.sh
+++ install.sh
@@ -330,7 +330,7 @@ if [[ "$DEV" == 1 ]]; then
   
   
   # clone sdscli package
-  install_dev_repo $OPS sdscli https://github.com/hysds/sdscli.git
+  install_dev_repo $OPS sdscli https://github.com/hysds/sdscli.git HC-567
   
   
   # clone grq2 package
